b12ce9f595038b000733f0dd10b1e515bc817eab
ichiasa-g-3 <ichiasa-g-3@example.com>
2025-11-25 11:19:47 +0900

    JDBCの一時的な無効化

--- src/main/java/com/ikeda/RentalSystemProjectApplication.java
+++ src/main/java/com/ikeda/RentalSystemProjectApplication.java
@@ -2,8 +2,15 @@ package com.ikeda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.JdbcTemplateAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	    DataSourceAutoConfiguration.class,
+	    DataSourceTransactionManagerAutoConfiguration.class,
+	    JdbcTemplateAutoConfiguration.class
+	})
 public class RentalSystemProjectApplication {
 
 	public static void main(String[] args) {
